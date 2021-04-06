275e179f6d8359a78c7dcda2e66c2f16b38ce08b
ota-yuto <79303376+ota-yuto@users.noreply.github.com>
2021-04-06 23:19:28 +0900

    課題提出テスト

--- JavaStudy/Task1_4.java
+++ JavaStudy/Task1_4.java
@@ -19,26 +19,23 @@ public class Task1_4{
 
         //②「name」の値が「USER_RNAME」と等しく、「pass」の値が「USER_PASSWORD」と等しい場合。
         //定数を使用して「ログイン成功です」と出力してください。
-        if(name.equals(USER_RNAME)&&pass.equals(USER_PASSWORD)){
+        if(name.equals(USER_RNAME) && pass.equals(USER_PASSWORD)){
             system.out.println(CONST_MSG_SUCCESS);
             system.out.prinntln(USER_RNAME);
         }
         //③「USER_RNAME」の値のみ等しい場合。
         //定数を使用して「パスワードに誤りがあります。」と出力してください。
-        if(name.equals(USER_RNAME)){
-        }else{
+        else if(name.equals(USER_RNAME)){
                 system.out.prinntln(CONST_MSG_ERROR_PASS);
         }
         //④「USER_PASSWORD」の値のみ等しい場合。
         //定数を使用して「入力情報に誤りがあります。」と出力してください。
-        if(pass.equals(USER_PASSWORD)){
-        }else{
-                system.out.prinntln(CONST_MSG_ERROR_INPUT);
-            }
+        else if(pass.equals(USER_PASSWORD)){
+            System.out.println(CONST_MSG_ERROR_INPUT);    
+        }
         //⑤「USER_RNAME」も「USER_PASSWORD」の値も間違っていた場合。
         //定数を使用して「入力情報に誤りがあります。」と出力してください。
-        if(name.equals(USER_RNAME)&&pass.equals(USER_PASSWORD)){
-        }else{
+        else{
                 system.out.prinntln(CONST_MSG_ERROR_INPUT);
             }    
         
